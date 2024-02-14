cd00f87487f7c385f612f91d5bb55e3fe95544e0
prakhar.srivastava4 <prakhar.srivastava@tiqets.com>
2024-02-14 23:06:15 +0100

    editor chanes

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -36,7 +36,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here 
   -- with the ones you want to install
-  ensure_installed = {'rust_analyzer', 'pyre', 'yamlls', 'pylsp'},
+  ensure_installed = {'rust_analyzer', 'yamlls', 'pylsp'},
   handlers = {
     lsp_zero.default_setup,
   },
