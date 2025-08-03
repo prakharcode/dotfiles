3fa9c8aa55af0aae5430bdd51393b3e5f7803d10
prakhar.srivastava4 <prakhar.srivastava@tiqets.com>
2025-08-03 21:30:47 +0200

    [feat]: Added ai for claude

--- nvim/lua/prakharcode/lazy_plugin_manager/plugins/cmp.lua
+++ nvim/lua/prakharcode/lazy_plugin_manager/plugins/cmp.lua
@@ -1,3 +1,8 @@
 return {
-  "hrsh7th/nvim-cmp",
+	"hrsh7th/nvim-cmp",
+	dependencies = { "hrsh7th/cmp-emoji" },
+	---@param opts cmp.ConfigSchema
+	opts = function(_, opts)
+		table.insert(opts.sources, { name = "emoji" })
+	end,
 }
